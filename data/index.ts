80d46a4b6b9945b6790f17a20e81e0b7b9702cea
RitanshRajput <it2042@global.org.in>
2024-06-15 12:38:05 +0530

    My portfolio Latest

---
+++ data/index.ts
@@ -0,0 +1,284 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a FullStack Web App",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "ThreeJS 3D UI",
+    des: "Explore the wonders of 3D animation using the Three.Js",
+    img: "/threejs.png",
+    iconLists: ["/re.svg", "/tail.svg", "/js.svg", "/three.svg", "/fm.svg"],
+    link: "https://react75-threejs-portfolio-14.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Speech-To-Text",
+    des: "An AI-powered speech-to-text converter that transcribes voice input and allows text copying.",
+    img: "/speech.png",
+    iconLists: ["/react.svg", "/tail.svg", "/js.svg", "/stream.svg", "/c.svg"],
+    link: "https://react75-speech-to-recognition-app-4.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Google Clone",
+    des: "A Google clone utilizing the Google API to fetch and display real-time data with a familiar UI.",
+    img: "/google.png",
+    iconLists: ["/react.svg", "/tail.svg", "/js.svg", "/three.svg", "/c.svg"],
+    link: "https://google-search-page-clone-one.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Chakra UI Video App",
+    des: "A video app created with React and Chakra UI, allowing users to upload and watch videos with dark and light mode options.",
+    img: "/chakraui.png",
+    iconLists: [
+      "/react.svg",
+      "/tail.svg",
+      "/js.svg",
+      "/three.svg",
+      "/gsap.svg",
+    ],
+    link: "https://chakra-ui-project-one.vercel.app/",
+  },
+  {
+    id: 5,
+    title: "Crypto Tracker",
+    des: "A cryptocurrency web app built with React.js and Chakra UI to display real-time cryptocurrency details.",
+    img: "/crypto.png",
+    iconLists: [
+      "/react.svg",
+      "/tail.svg",
+      "/js.svg",
+      "/three.svg",
+      "/gsap.svg",
+    ],
+    link: "https://crpytocurrency-app-using-chakra-ui.vercel.app/",
+  },
+  {
+    id: 6,
+    title: "Kanban Board",
+    des: "A Kanban board application created with React.js external Kanban Library",
+    img: "/kanban.png",
+    iconLists: [
+      "/react.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/three.svg",
+      "/gsap.svg",
+    ],
+    link: "https://react75-kanban-board-3.vercel.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Ritansh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ritansh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ritansh is the ideal partner.",
+    name: "Yuvraj Singh Lodhi",
+    title: "FullStack Developer at Sumati.io",
+  },
+  {
+    quote:
+      "Collaborating with Ritansh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ritansh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ritansh is the ideal partner.",
+    name: "Alok Kumar urmaliya",
+    title: "FullStack Developer at HotSpring",
+  },
+  {
+    quote:
+      "Collaborating with Ritansh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ritansh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ritansh is the ideal partner.",
+    name: "Rupesh Kachhi",
+    title: "Software Developer at Sumati.io",
+  },
+  {
+    quote:
+      "Collaborating with Ritansh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ritansh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ritansh is the ideal partner.",
+    name: "Yuvraj Singh Lodhi",
+    title: "FullStack Developer at Sumati.io",
+  },
+  {
+    quote:
+      "Collaborating with Ritansh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ritansh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ritansh is the ideal partner.",
+    name: "Alok Kumar urmaliya",
+    title: "FullStack Developer at HotSpring",
+  },
+];
+
+export const technologyAndTools = [
+  {
+    id: 1,
+    name: "JavaScript",
+    img: "/js.svg",
+    nameImg: "/jsName.svg",
+  },
+  {
+    id: 2,
+    name: "ReactJS",
+    img: "/react.svg",
+    nameImg: "/reactName.svg",
+  },
+  {
+    id: 3,
+    name: "MongoDB",
+    img: "/mongo.png",
+    nameImg: "/mongoName.svg",
+  },
+  {
+    id: 4,
+    name: "NodeJS",
+    img: "/node.svg",
+    nameImg: "/nodeName.svg",
+  },
+  {
+    id: 5,
+    name: "NextJS",
+    img: "/nextjs.svg",
+    nameImg: "/nextName.svg",
+  },
+  {
+    id: 6,
+    name: "Tailwind",
+    img: "/tailwind.svg",
+    nameImg: "/tailwindName.svg",
+  },
+  {
+    id: 7,
+    name: "Postgresql",
+    img: "/postgresql.svg",
+    nameImg: "/postgresqlName.svg",
+  },
+  {
+    id: 8,
+    name: "CANVA",
+    img: "/canva.svg",
+    nameImg: "/canvaName.svg",
+  },
+  {
+    id: 9,
+    name: "VERCEL",
+    img: "/vercel.svg",
+    nameImg: "/vercelName.svg",
+  },
+  {
+    id: 10,
+    name: "GITHUB",
+    img: "/github.svg",
+    nameImg: "/githubName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "FullStack Development",
+    desc: "Practicing new Development method to enhancing interactivity using MERN Stack and other Tools and Technology.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Open Source Contribution",
+    desc: "Contributed in Open Source Project to demonstrate and Learn for Different Unique Projects",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance Web Dev Project",
+    desc: "collaborated with a team of developer for a client, from frontend to Backend using Javascript, React JS, NodeJS",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Team Lead At Major Project in College",
+    desc: "Developed and maintained user-friendly features using modern frontend and Backend technologies with My Team.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/RitanshRajput",
+  },
+  {
+    id: 2,
+    img: "/insta.svg",
+    link: "https://www.instagram.com/damndudedd/",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://linkedin.com/in/ritansh-rajput",
+  },
+];
